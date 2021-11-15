cb87ac738afb01dd1107f3804c2b1237084466c6
xxxue <1659809758@qq.com>
2021-11-15 21:11:13 +0800

    改为.net6

--- SearchPort/Program.cs
+++ SearchPort/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
 namespace SearchPort
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
-        {        
+        private static void Main(string[] args)
+        {
             while (true)
             {
-                var res = GetPortInfo();
-                if (res.Item1)
+                var (result, portInfoArr) = GetPortInfo();
+                if (result)
                 {
-                    ShowProtInfo(res.Item2);
+                    ShowPortInfo(portInfoArr);
                 }
+
                 Console.WriteLine("是否退出?(y/n)");
                 var inputValue = Console.ReadKey();
                 if (inputValue.Key == ConsoleKey.Y)
@@ -24,50 +26,57 @@ namespace SearchPort
             }
         }
 
-        public static Tuple<bool, string[]> GetPortInfo()
+        /// <summary>
+        /// 获取端口信息
+        /// </summary>
+        /// <returns></returns>
+        private static Tuple<bool, string[]> GetPortInfo()
         {
-
             Console.WriteLine("请输入端口号:(Enter键确认)");
+            // 读取用户输入的端口号
             var inputPort = Console.ReadLine();
             if (string.IsNullOrEmpty(inputPort))
             {
                 Console.WriteLine("端口号为空,请检查.");
-                return new Tuple<bool, string[]>(false, new string[1]);
+                return new Tuple<bool, string[]>(false, null);
             }
+
+            // 拼接命令
             var cmdStr = "netstat -ano | findstr " + inputPort;
 
+            // 执行命令,获取返回值
             var cmdRes = RunCmd(cmdStr);
 
+            // 格式化
             var arr = cmdRes.Split(" ");
+            
             arr = arr
-                 .Where(a => !string.IsNullOrWhiteSpace(a))
-                 .Select(a =>
-                 {
-                     return a.Replace("\r\n", "");
-                 }).ToArray();
+                .Where(a => !string.IsNullOrWhiteSpace(a))
+                .Select(a => a.Replace("\r\n", "")).ToArray();
 
             if (arr.Length == 0)
             {
                 Console.WriteLine("该端口没有被程序占用.");
-                return new Tuple<bool, string[]>(false, new string[1]);
-
+                return new Tuple<bool, string[]>(false, null);
             }
 
-
             return new Tuple<bool, string[]>(true, arr);
-
         }
 
-        public static void ShowProtInfo(string[] arr)
+        /// <summary>
+        /// 显示端口信息
+        /// </summary>
+        /// <param name="arr"></param>
+        private static void ShowPortInfo(IReadOnlyList<string> arr)
         {
-            for (int i = 0; i < arr.Length; i = i + 5)
+            for (var i = 0; i < arr.Count; i += 5)
             {
-                
                 // 防止数组越界 与 数据过多
-                if (i + 4 > arr.Length || i + 3 > arr.Length || i + 2 > arr.Length || i + 1 > arr.Length || i > 80)
+                if (i + 4 > arr.Count || i + 3 > arr.Count || i + 2 > arr.Count || i + 1 > arr.Count || i > 80)
                 {
                     break;
                 }
+
                 Console.WriteLine("-----------------");
                 Console.WriteLine("PID:  " + arr[i + 4]);
                 Console.WriteLine("\t类型:" + arr[i]);
@@ -77,10 +86,15 @@ namespace SearchPort
             }
         }
 
-        public static string RunCmd(string cmd)
+        /// <summary>
+        /// 执行命令
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        private static string RunCmd(string cmd)
         {
             cmd = cmd.Trim().TrimEnd('&') + "&exit";
-            Process process = new Process();
+            var process = new Process();
             process.StartInfo.CreateNoWindow = true;
             process.StartInfo.FileName = "cmd.exe";
             process.StartInfo.UseShellExecute = false;
@@ -91,12 +105,15 @@ namespace SearchPort
             process.StandardInput.WriteLine(cmd);
             process.StandardInput.AutoFlush = true;
 
-            string outStr = process.StandardOutput.ReadToEnd();
+            var outStr = process.StandardOutput.ReadToEnd();
             process.WaitForExit();
             process.Close();
 
-            outStr = outStr.Substring(outStr.IndexOf(cmd) + cmd.Length + "\r\n".Length);
+            var startStrLength = outStr.IndexOf(cmd) + cmd.Length + "\r\n".Length;
+            // 截取
+            outStr = outStr[startStrLength..];
             return outStr;
         }
+
     }
 }
